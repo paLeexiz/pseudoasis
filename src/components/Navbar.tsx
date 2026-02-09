c6dd33513b10a2ddc6bad0e2bc076bd744609778
paLeexiz <abaalv19@outlook.com>
2026-02-08 21:31:29 -0600

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
 
         <script async src="https://cse.google.com/cse.js?cx=47f3488f9331244f1">
         </script>
-        <div className="gcse-search"></div>
+        <div className="gcse-search text-black"></div>
       </div>
 
       <div className="flex items-center gap-10">
@@ -82,4 +82,5 @@ export default function Navbar() {
     </nav>
   );
 }
+
 
